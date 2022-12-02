6b8cd2cbd0cddc63997ab63fa1805d3f2b08af13
Tepthonee <115221367+Tepthonee@users.noreply.github.com>
2022-12-02 11:34:52 +0300

    Update الفحص.py

--- sbb_b/plugins/الفحص.py
+++ sbb_b/plugins/الفحص.py
@@ -36,7 +36,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**سورس كرستين يعمل بنجاح**"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**سورس تيبثون يعمل بنجاح**"
     JMTHON_IMG = gvarstatus("ALIVE_PIC")
     caption = jmthon_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
@@ -73,7 +73,7 @@ async def amireallyalive(event):
 temp = """{ALIVE_TEXT}
 **{EMOJI} قاعدة البيانات :** `{dbhealth}`
 **{EMOJI} اصدار التيليثون:** `{telever}`
-**{EMOJI} اصدار كرستين  :** `{jmver}`
+**{EMOJI} اصدار تيبثون  :** `{jmver}`
 **{EMOJI} اصدار البايثون :** `{pyver}`
 **{EMOJI} الوقت :** `{uptime}`
 **{EMOJI} المالك:** {mention}"""
@@ -81,9 +81,9 @@ temp = """{ALIVE_TEXT}
 
 def jmthonalive_text():
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
-    jmthon_caption = "**سورس كرستين يعمل بنجاح**\n"
+    jmthon_caption = "**سورس تيبثون يعمل بنجاح**\n"
     jmthon_caption += f"**{EMOJI} اصدار التيليثون :** `{version.__version__}\n`"
-    jmthon_caption += f"**{EMOJI} اصدار كرستين :** `{jmthonversion}`\n"
+    jmthon_caption += f"**{EMOJI} اصدار تيبثون :** `{jmthonversion}`\n"
     jmthon_caption += f"**{EMOJI} اصدار البايثون :** `{python_version()}\n`"
     jmthon_caption += f"**{EMOJI} المالك:** {mention}\n"
     return jmthon_caption
@@ -99,12 +99,12 @@ async def repo(event):
     await event.delete()
 
 
-ROZ_PIC = "https://telegra.ph/file/e31fcd025ddc59341c4a1.jpg"
+ROZ_PIC = "https://telegra.ph/file/f1e757035e56613a9ef92.jpg"
 RAZAN = Config.TG_BOT_USERNAME
 ROZ_T = (
-    f"**⌯︙بوت كرستين يعمل بنجاح 🤍،**\n"
+    f"**⌯︙بوت تيبثون يعمل بنجاح 🤍،**\n"
     f"**   - اصدار التليثون :** `1.23.0\n`"
-    f"**   - اصدار كرستين :** `4.0.0`\n"
+    f"**   - اصدار تيبثون :** `4.0.0`\n"
     f"**   - البوت المستخدم :** `{RAZAN}`\n"
     f"**   - اصدار البايثون :** `3.9.6\n`"
     f"**   - المستخدم :** {mention}\n"
@@ -121,8 +121,8 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         if query.startswith("السورس") and event.query.user_id == sbb_b.uid:
             buttons = [
                 [
-                    Button.url("قنـاة السـورس ⚙️", "https://t.me/cr_source"),
-                    Button.url("المطـور 👨🏼‍💻", "https://t.me/cr_cr_cr"),
+                    Button.url("قنـاة السـورس ⚙️", "https://t.me/Tepthone"),
+                    Button.url("المطـور 👨🏼‍💻", "https://t.me/PPF22"),
                 ]
             ]
             if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
